a70c7705e760f1fa20a2e34efb83fe2076e3191b
Karma _Andrew_ <karma.andrew16@gmail.com>
2023-11-24 19:06:39 +0200

    update

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,7 +1,7 @@
 import config from '.'
 export const connectionOptions = {
-  autoIndex: config.NODE_ENV !== 'production', // Don't build indexes in production
-  serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds of inactivity during server selection
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
+  autoIndex: config.NODE_ENV !== 'production', // dont build indexes in production
+  serverSelectionTimeoutMS: 5000, // timeout after 5 seconds of inactivity during server selection
+  socketTimeoutMS: 45000, // close sockets after 45 seconds of inactivity
+  family: 4, // use IPv4, skip trying IPv6
 }

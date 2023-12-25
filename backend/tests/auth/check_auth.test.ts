01f56287c889cc13f63ec43bc34d13fe803ecf7e
Karma _Andrew_ <karma.andrew16@gmail.com>
2023-12-25 12:07:43 +0200

    added more tests

---
+++ backend/tests/auth/check_auth.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from '@jest/globals'
+import request from 'supertest'
+import { app } from '../setup'
+
+describe('Simple login test', () => {
+  test('should return 400 after login', async () => {
+    const res = await request(app).post('/check-auth')
+    expect(res.body.data.message).toBe('User not loggedIn')
+  })
+})
--- backend/tests/auth/login.test.ts
+++ backend/tests/auth/login.test.ts
@@ -1,5 +1,6 @@
 import { beforeAll, describe, expect, test } from '@jest/globals'
 import request from 'supertest'
+import { User } from '../../src/models/user'
 import { app, user } from '../setup'
 beforeAll(async () => {
   const data = {
@@ -39,5 +40,106 @@ describe('Simple login test', () => {
     expect(secondRequestRes.body.data.loggedIn).toBe(true)
 
     expect(secondRequestRes.body.data.message).toBe('User logged in')
+  })
+})
+
+describe('Simple login test', () => {
+  test('should return 200 after login', async () => {
+    const loginData = {
+      email: `${user}@gmail.com`,
+      password: 'ssszzsA37a!',
+      firstName: 'dsaddzsadsa',
+      confirm_password: 'ssszzsA37a!',
+      lastName: 'dsaddsadsa',
+      gender: 'Female',
+      username: user,
+      rememberMe: false,
+    }
+
+    const loginRes = await request(app).post('/login').send(loginData)
+    expect(loginRes.status).toEqual(200)
+
+    const cookie = loginRes.headers['set-cookie']
+
+    const secondRequestRes = await request(app)
+      .post('/check-auth')
+      .set('Cookie', cookie)
+
+    expect(secondRequestRes.body.data.loggedIn).toBe(true)
+
+    expect(secondRequestRes.body.data.message).toBe('User logged in')
+  })
+})
+
+describe('Simple login test, user not exists any more...', () => {
+  test('should return 400 after login', async () => {
+    const loginData = {
+      email: `${user}@gmail.com`,
+      password: 'ssszzsA37a!',
+      firstName: 'dsaddzsadsa',
+      confirm_password: 'ssszzsA37a!',
+      lastName: 'dsaddsadsa',
+      gender: 'Female',
+      username: user,
+      rememberMe: false,
+    }
+
+    const loginRes = await request(app).post('/login').send(loginData)
+    expect(loginRes.status).toEqual(200)
+
+    const cookie = loginRes.headers['set-cookie']
+    await User.deleteOne({ username: user })
+    const secondRequestRes = await request(app)
+      .post('/check-auth')
+      .set('Cookie', cookie)
+
+    expect(secondRequestRes.body.data.loggedIn).toBe(false)
+    expect(secondRequestRes.body.data.message).toBe(
+      'User Successfully Logged Out'
+    )
+
+    const data = {
+      email: `${user}@gmail.com`,
+      password: 'ssszzsA37a!',
+      firstName: 'dsaddzsadsa',
+      confirm_password: 'ssszzsA37a!',
+      lastName: 'dsaddsadsa',
+      gender: 'Female',
+      username: user,
+      rememberMe: true,
+    }
+    await request(app).post('/register').send(data)
+  })
+})
+
+describe('Simple login test', () => {
+  test('should return 200 after login', async () => {
+    await User.updateOne({ username: user }, { $set: { ip: [] } })
+    const loginData = {
+      email: `${user}@gmail.com`,
+      password: 'ssszzsA37a!',
+      firstName: 'dsaddzsadsa',
+      confirm_password: 'ssszzsA37a!',
+      lastName: 'dsaddsadsa',
+      gender: 'Female',
+      username: user,
+      rememberMe: false,
+    }
+
+    const loginRes = await request(app).post('/login').send(loginData)
+
+    expect(loginRes.body.message).toBe('Verify your email')
+    await User.deleteOne({ username: user })
+    const data = {
+      email: `${user}@gmail.com`,
+      password: 'ssszzsA37a!',
+      firstName: 'dsaddzsadsa',
+      confirm_password: 'ssszzsA37a!',
+      lastName: 'dsaddsadsa',
+      gender: 'Female',
+      username: user,
+      rememberMe: true,
+    }
+    await request(app).post('/register').send(data)
   })
 })
